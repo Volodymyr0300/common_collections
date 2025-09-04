60d37189fa85c9df16cf779e2a876994662b234f
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-09-04 18:41:47 -0500

    creat_upd_drop_of_vec

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,21 @@
 fn main() {
-    // Iterators can be collected into vectors
-    let collected_iterator: Vec<i32> = (0..10).collect();
-    println!("Collected (0..10) into: {:?}", collected_iterator);
+    let v1: Vec<i32> = Vec::new();
+    println!("Empty vector: {:?}", v1);
+    
+    let v2 = vec![1, 2, 3];
+    println!("Empty vector: {:?}", v2);
 
-    // The `vec!` macro can be used to initialize a vector
-    let mut xs = vec![1i32, 2, 3];
-    println!("Initial vector: {:?}", xs);
+    let mut v3 = Vec::new();
+    v3.push(5);
+    v3.push(6);
+    v3.push(7);
+    v3.push(8);
+    println!("Vector after pushes: {:?}", v3);
 
-    // Insert new element at the end of the vector
-    println!("Push 4 into the vector");
-    xs.push(4);
-    println!("Vector: {:?}", xs);
+    {
+        let v = vec![1, 2, 3, 4];
+        // do stuff with v
+    } // <- v goes out of scope and is freed here   
 
-    // Error! Immutable vectors can't grow
-    //! collected_iterator.push(0);
-    // FIXME ^ Comment out this line
-
-    // The `len` method yields the number of elements currently stored in a vector
-    println!("Vector length: {}", xs.len());
-
-    // Indexing is done using the square brackets (indexing starts at 0)
-    println!("Second element: {}", xs[1]);
-
-    // `pop` removes the last element from the vector and returns it
-    println!("Pop last element: {:?}", xs.pop());
-
-    // Out of bounds indexing yields a panic
-    //! println!("Fourth element: {}", xs[3]);
-    // FIXME ^ Comment out this line
-
-    // `Vector`s can be easily iterated over
-    println!("Contents of xs:");
-    for x in xs.iter() {
-        println!("> {}", x);
-    }
-
-    // A `Vector` can also be iterated over while the iteration
-    // count is enumerated in a separate variable (`i`)
-    for (i, x) in xs.iter().enumerate() {
-        println!("In position {} we have value {}", i, x);
-    }
-
-    // Thanks to `iter_mut`, mutable `Vector`s can also be iterated
-    // over in a way that allows modifying each value
-    for x in xs.iter_mut() {
-        *x *= 3;
-    }
-    println!("Updated vector: {:?}", xs);
+    
 }
