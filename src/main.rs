3486e4325c3b7f38ab30bf34c836996b0d7883f9
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-09-04 19:30:38 -0500

    string

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,14 @@
 fn main() {
-    let v = vec![1, 2, 3, 4, 5];
-    let third: &i32 = &v[2];
-    println!("The third element is {}", third);
+    let mut s = String::new(); // Create a new empty String
     
-    match v.get(2) {
-        Some(third) => println!("The third element is {}", third),
-        None => println!("There is no third element."),
-    }
 
-    // ! This will cause a panic
-    // let does_not_exist = &v[100];
-    // println!("does_not_exist: {:?}", does_not_exist);
-    // let does_not_exist = v.get(100);
-    // println!("does_not_exist: {:?}", does_not_exist);
+    let data = "initial contents";
+    let s = data.to_string();
+    // the method also works on a literal directly:
+    let s = "initial contents".to_string(); 
+    // Using the to_string method to create a String from a string literal
 
-    // ! Iterating over the values in a vector
-    // let mut v = vec![1, 2, 3, 4, 5];
-    // let first = &v[0];
-    // v.push(6);
-    // println!("The first element is: {}", first);
-
-    let v = vec![100, 32, 57];
-    for i in &v {
-        println!("{}", i);
-    }
+    let mut s = String::from("foo");
+    s.push_str("bar");
+    println!("s: {}", s); // s: foobar
 }
