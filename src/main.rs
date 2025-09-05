2fb863c5f9f63b9b20e2dc97b9985ba4d9132807
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-09-04 19:59:59 -0500

    append_concat_indexing_str

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,31 @@
 fn main() {
+    /* String Concatenation 
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2; // note s1 has been moved here and can no longer be used
+    */
 
 
-    let mut s = String::from("foo");
-    s.push_str("bar");
-    println!("s: {}", s); // s: foobar
+    /*
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+    let s = s1 + "-" + &s2 + "-" + &s3;
+     */
 
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+    let s = format!("{}-{}-{}", s1, s2, s3);
+    // s1, s2, and s3 are still valid here because format! does not take ownership
+   println!("Formatted string: {}", s);
+   println!("Original strings: {}, {}, {}", s1, s2, s3);
+   
+    for c in "Hello".chars() {
+        println!("{}", c);
+    }
+    for b in "Hello".bytes() {
+        println!("{}", b);
+    }
 
-    let mut s1 = String::from("foo");
-    let s2 = "bar";
-    s1.push_str(s2);
-    println!("s1 is {}", s1);
-    println!("s2 is {}", s2);
-
-    let mut s = String::from("lo");
-    s.push('l');
-    println!("s: {}", s); // s: lol
 }
