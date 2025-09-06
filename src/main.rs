3c5852ba69968b749733d5a32b6540ea93c986cd
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-09-05 21:11:34 -0500

    counting_sum_in_vec

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,10 @@
+
+
 fn main() {
-    use std::collections::HashMap;
+    let data = vec![5,1,2,2,5,4,3,1,4,5,5];
     
-    let mut scores = HashMap::new();
-    scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Yellow"), 50);
-    println!("Scores: {:?}", scores);
+    let sum: i32 = data.iter().sum();
     
-    
-    let teams = vec![String::from("Blue"), String::from("Yellow")];
-    let initial_scores = vec![10, 50];
-    let scores2: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
-    println!("Scores2: {:?}", scores2);
-
-    let field_name = String::from("Favorite color");
-    let field_value = String::from("Blue");
-    let mut map = HashMap::new();
-    map.insert(field_name, field_value);
-    // field_name and field_value are invalid at this point, try using them and
-    // see what compiler error you get!
-    println!("Map: {:?}", map);
-    
-    for (key, value) in &scores {
-        println!("{}: {}", key, value);
-    }
-
-    let mut scores = HashMap::new();
-    scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Yellow"), 50);
-    let team_name = String::from("Blue");
-    let score = scores.get(&team_name);
-    println!("Score for {}: {:?}", team_name, score);
-
-    for (key, value) in &scores {
-        println!("{}: {}", key, value);
-    }
-
-    scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Blue"), 25);
-    println!("Scores after update: {:?}", scores);
-
-    scores.insert(String::from("Blue"), 10);
-    scores.entry(String::from("Yellow")).or_insert(50);
-    scores.entry(String::from("Blue")).or_insert(50);
-    println!("{:?}", scores);
-
-    let text = "hello world wonderful world";
-    let mut map = HashMap::new();
-    for word in text.split_whitespace() {
-        let count = map.entry(word).or_insert(0);
-        *count += 1;
-    }
-    println!("{:?}", map);
+    println!("Mean: {}", sum as f64 / data.len() as f64);
 }
+
